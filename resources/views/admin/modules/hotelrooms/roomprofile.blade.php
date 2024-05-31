58c0d17bc5950d6cea0318194e958f7870ee7096
laxman99 <parajulil385@gmail.com>
2024-05-31 13:50:59 +0545

    Updated in the room profile

--- resources/views/admin/modules/hotelrooms/roomprofile.blade.php
+++ resources/views/admin/modules/hotelrooms/roomprofile.blade.php
@@ -4,15 +4,7 @@
 <section style="background-color: #eee;">
     <div class="container py-5">
         <div class="row">
-            <div class="col">
-                <nav aria-label="breadcrumb" class="bg-body-tertiary rounded-3 p-3 mb-4">
-                    <ol class="breadcrumb mb-0">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item"><a href="#">User</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">{{$roomData->title}}</li>
-                    </ol>
-                </nav>
-            </div>
+
         </div>
 
         <div class="row">
